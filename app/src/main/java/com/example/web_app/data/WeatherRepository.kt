b26552b123599bb657cbf41673067237175893d7
GadelevMaxim <maximgadelev@mail.ru>
2022-03-02 19:31:01 +0300

    "forget add interceptor"

--- app/src/main/java/com/example/web_app/data/WeatherRepository.kt
+++ app/src/main/java/com/example/web_app/data/WeatherRepository.kt
@@ -4,12 +4,14 @@ import androidx.viewbinding.BuildConfig
 import com.example.web_app.data.api.Api
 import com.example.web_app.data.response.WeatherList
 import com.example.web_app.data.response.WeatherResponse
+import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-
+private const val QUERY_UNITS = "units"
+private const val UNITS = "metric"
 private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 private const val API_KEY = "9e8331105a420290e0619d9769c5dc65"
 private const val QUERY_API_KEY = "appid"
@@ -21,6 +23,18 @@ WeatherRepository {
         val original = chain.request()
         val newURL = original.url.newBuilder()
             .addQueryParameter(QUERY_API_KEY, API_KEY)
+            .build()
+
+        chain.proceed(
+            original.newBuilder()
+                .url(newURL)
+                .build()
+        )
+    }
+    private val unitsInterceptor = Interceptor { chain ->
+        val original = chain.request()
+        val newURL: HttpUrl = original.url.newBuilder()
+            .addQueryParameter(QUERY_UNITS, UNITS)
             .build()
 
         chain.proceed(
